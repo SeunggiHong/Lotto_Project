033abb26f4cf597da48525200c804dbbb72d8316
SeunggiHong <tmdrl1026@naver.com>
2017-11-25 16:54:35 +0900

    17-11-25
    1

--- app/src/main/java/com/example/hsg/lotto_project/RandomActivity.java
+++ app/src/main/java/com/example/hsg/lotto_project/RandomActivity.java
@@ -1,7 +1,11 @@
 package com.example.hsg.lotto_project;
 
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -12,7 +16,13 @@ import org.w3c.dom.Text;
 
 public class RandomActivity extends AppCompatActivity {
 
-    private Button makeBtn;
+    private MySQLiteOpenHelper helper;
+    String dbName = "random_.db";
+    int dbVersion = 1;
+    private SQLiteDatabase db;
+    String tag = "SQLite";
+
+    private Button makeBtn,saveBtn;
     private LinearLayout layout_number;
     private TextView[] tv_numbers;
 
@@ -22,6 +32,24 @@ public class RandomActivity extends AppCompatActivity {
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("번호 랜덤생성");
         setContentView(R.layout.activity_random_num);
+
+
+
+        helper = new MySQLiteOpenHelper(
+                this,
+                dbName,
+                null,
+                dbVersion);
+
+        try {
+            db = helper.getWritableDatabase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Log.e(tag, "DB error");
+            e.printStackTrace();
+            finish();
+
+        }
 
         layout_number = (LinearLayout) findViewById(R.id.lottolayout);
 
@@ -34,6 +62,7 @@ public class RandomActivity extends AppCompatActivity {
         }
 
         makeBtn = (Button) findViewById(R.id.makeBtn);
+        saveBtn = (Button) findViewById(R.id.saveBtn);
 
         makeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +71,57 @@ public class RandomActivity extends AppCompatActivity {
 
             }
         });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+
+//            String sql = "create table random_list(" +
+//                    "id seq primary key AUTOINCREMENT" +
+//                    " int l1m int l2, int l3, int l4, int l6,date text NOT NULL );";
+            @Override
+            public void onClick(View v) {
+
+           String sql = "insert into random_list " + "values(null,'" +tv_numbers[0].getText().toString()
+                                                                    +"','"+tv_numbers[1].getText().toString()
+                                                                    +"','"+tv_numbers[2].getText().toString()
+                                                                    +"','"+tv_numbers[3].getText().toString()
+                                                                    +"','"+tv_numbers[4].getText().toString()
+                                                                    +"','"+tv_numbers[5].getText().toString()
+                                                                    +"');";
+
+                Log.e(tag,""+sql);
+
+            db.execSQL(sql);
+
+
+            }
+        });
+
+        //셀렉트
+        SelectNum();
+
+    }
+
+    private void SelectNum(){
+        String sql = "select * from random_list;";
+
+        Cursor result = db.rawQuery(sql,null);
+        result.moveToFirst();
+        while(!result.isAfterLast()){
+            int id = result.getInt(0);
+            String voca = result.getString(1);
+
+            Toast.makeText(this,"seq= "+id+  " 1: " +result.getString(1) +" 2: " +result.getString(2) +" 3: " +result.getString(3) +" 4: " +result.getString(4), Toast.LENGTH_LONG).show();
+            result.moveToNext();
+
+        }
+        result.close();
+
+//        while(c.moveToNext()) {
+//            int id = c.getInt(0);
+//            String name = c.getString(1);
+//            Log.d(tag,"id:"+id+",name:"+name);
+//        }
+
 
     }
 
@@ -52,49 +132,29 @@ public class RandomActivity extends AppCompatActivity {
         int temp = 0;
 
         for(int i=0; i<lottoNums.length; i++) {
-
             lottoNums[i] = (int) (Math.random() * 45) + 1;
-
             //중복제거
             for(int j=i-1;j>=0;j--){
-
                 if(lottoNums[i]==lottoNums[j]){
                     i--;
                     break;
                 }
-
             }
         }
-
         //오름차순으로 sort
         for(int i=0;i<lottoNums.length;i++) {
-
             for (int j = 0; j < lottoNums.length; j++) {
-
                 if (lottoNums[i] < lottoNums[j]) {
-
                     temp = lottoNums[i];
-
                     lottoNums[i] = lottoNums[j];
-
                     lottoNums[j] = temp;
-
                 }
-
             }
         }
-
         for(int i=0; i<lottoNums.length; i++){
-
             tv_numbers[i].setText(""+lottoNums[i]);
-//            Toast.makeText(getApplicationContext(), ""+lottoNums[i],Toast.LENGTH_SHORT).show();
-
-
         }
-
     }
-
-
 
 
 
